34da48ac8e05bbc1afaabb70a969420e9ef9a666
Amir Fakheraldeen <amir.f.aldeen@gmail.com>
2017-09-15 23:36:29 +0300

    Add file metadata microservice front end

---
+++ client/src/containers/FileUpload.jsx
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import Ajax from '../js/ajax';
+
+export default class FileUpload extends Component {
+  constructor() {
+    super();
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  render() {
+    return (
+      <section>
+        <h2>File Metadata Microservice</h2>
+        <p>
+          Allows you to upload a file to the server and returns the file's size.
+        </p>
+        <form method='post' action='/api/file-upload' onSubmit={this.handleSubmit}>
+          <input type='file' name='file' />
+          <input type='submit' value='Upload' />
+        </form>
+      </section>
+    );
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+
+    let url = event.target.getAttribute('action');
+    let options = {
+      headers: [
+        { 'Content-type': 'application/x-www-form-urlencoded' }
+      ],
+      form: new FormData(event.target)
+    };
+
+    Ajax.
+      post(url, options).
+      then(
+        function fulfilled(response) {
+          console.log(response);
+        },
+        function rejected(reason) {
+          console.error('rejected');
+        });
+  }
+};
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -5,6 +5,7 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import Header from './containers/Header.jsx';
 import Timestamp from './containers/Timestamp.jsx';
 import HeaderParser from './containers/HeaderParser.jsx';
+import FileUpload from './containers/FileUpload.jsx';
 
 ReactDOM.render((
   <BrowserRouter>
@@ -12,6 +13,7 @@ ReactDOM.render((
       <Header></Header>
       <Route path='/timestamp' component={Timestamp}></Route>
       <Route path='/header-parser' component={HeaderParser}></Route>
+      <Route path='/file-upload' component={FileUpload}></Route>
     </div>
   </BrowserRouter>
   ),
